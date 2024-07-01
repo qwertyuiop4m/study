e76a12eb7eb4cc598b0e67b6961c8f4cd1b4f445
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-07-01 17:49:36 +0900

    jpa study day1.2

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,16 +1,25 @@
 package hellojpa;
 
 import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
     @Column(name = "name",nullable = false)
     private String username;
 
-    public Member() {
+    public Member(){
     }
 
     public Long getId() {
