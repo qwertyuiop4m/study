4ca9e52469d14964ffe8f16aeae5b727b871b69a
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-07-24 17:40:44 +0900

    front controllerV3-model추가

--- servlet/src/main/java/hello/servlet/Web/frontcontroller/Myview.java
+++ servlet/src/main/java/hello/servlet/Web/frontcontroller/Myview.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.rmi.ServerException;
+import java.util.Map;
 
 public class Myview {
     private String viewPath;
@@ -18,5 +19,15 @@ public class Myview {
     public void render(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request,response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request,response);
+    }
+    private void modelToRequestAttribute(Map<String, Object> model,
+                                         HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
