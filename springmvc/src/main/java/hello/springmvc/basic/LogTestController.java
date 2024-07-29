3b6c565f98b967fd47fc89859199662f9f88bbee
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-07-29 18:32:34 +0900

    mapping

--- springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+//@Slf4j
 @RestController
 public class LogTestController {
 
