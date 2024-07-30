0f59e02d27d3efed8354b9e877ed157955f8f7d1
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-07-30 18:41:17 +0900

    product details

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,9 +29,16 @@ public class BasicItemController {
 
     }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable("itemId") Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
     @PostConstruct
     public void init(){
         itemRepository.save(new Item("itemA",10000,10));
-        itemRepository.save(new Item("itemA",20000,20));
+        itemRepository.save(new Item("itemB",20000,20));
     }
 }
