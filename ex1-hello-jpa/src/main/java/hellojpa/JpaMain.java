c838cf501244a5acff05d89358902b0298a55712
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-08-07 19:42:58 +0900

    jpql

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,37 +19,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member=new Member();
-            member.setName("member1");
-            member.setHomeAddress(new Address("homeCity","street","100000"));
+            List<Member> result = em.createQuery(
+                    "select m From Member m where m.username like ‘%hello%'",
+                    Member.class
+            ).getResultList();
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressesHistory().add(new AddressEntity("old1","street","100000"));
-            member.getAddressesHistory().add(new AddressEntity("old2","street","100000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("=============start==================");
-            Member findMember = em.find(Member.class, member.getId());
-
-            //homecity->newcity
-                //findMember.getHomeAddress().setCity("newCity");
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity",a.getStreet(),a.getZipcode()));
-
-            //치킨->한식
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-
-//            findMember.getAddressesHistory().remove(new AddressEntity("old1","street","100000"));
-//            findMember.getAddressesHistory().add(new AddressEntity("newCity1","street","100000"));
+            for (Member member : result) {
+                System.out.println("member = " + member);
+            }
 
             tx.commit();
         }catch (Exception e){
---
+++ jpql/src/main/java/org/example/jpql/JpaMain.java
@@ -0,0 +1,46 @@
+package org.example.jpql;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member=new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+            
+            em.flush();
+            em.clear();
+
+            List<MemberDTO> result = em.createQuery("select new org.example.jpql.MemberDTO(m.username,m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO=result.get(0);
+            System.out.println("memberDTO.username = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
+
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+        
+        emf.close();
+    }
+    
+}
