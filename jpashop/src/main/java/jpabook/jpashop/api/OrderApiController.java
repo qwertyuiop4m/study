d2818f822be19d66566d5d32a60fdeca48e589a5
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-08-27 14:04:10 +0900

    orders-v2(entity->DTO(fetch join(paging))

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -52,6 +53,17 @@ public class OrderApiController {
                 .map(OrderDto::new)
                 .collect(toList());
       return result;
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset",defaultValue = "0") int offset,
+            @RequestParam(value = "limit",defaultValue = "100")int limit) {
+        List<Order> orders=orderRepository.findAllWithMemberDelivery(offset,limit);
+        List<OrderDto> result = orders.stream()
+                .map(OrderDto::new)
+                .collect(toList());
+        return result;
     }
 
     @Data
--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -116,6 +116,7 @@ public class OrderRepository {
         ).getResultList();
     }
 
+
     public List<OrderSimpleQueryDto> findOrderDtos() {
        return em.createQuery(
                 "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
@@ -133,5 +134,13 @@ public class OrderRepository {
                         " join fetch o.orderItems oi" +
                         " join fetch oi.item i", Order.class)
                 .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m"+
+                        " join fetch o.delivery d", Order.class
+        ).setFirstResult(offset).setMaxResults(limit).getResultList();
     }
 }
