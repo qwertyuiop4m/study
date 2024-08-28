cac0a6310ca3cc8906b841e0355bbfad1ae191cb
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-08-28 18:21:53 +0900

    common interface

--- data-jpa/src/main/java/study/data_jpa/repository/TeamRepository.java
+++ data-jpa/src/main/java/study/data_jpa/repository/TeamRepository.java
@@ -1,39 +1,7 @@
 package study.data_jpa.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
-import study.data_jpa.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import study.data_jpa.entity.Team;
 
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class TeamRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public Team save(Team team){
-        em.persist(team);
-        return team;
-    }
-
-    public void delete(Team team){
-        em.remove(team);
-    }
-
-    public List<Team> findAll(){
-        return em.createQuery("select t from Team t",Team.class).getResultList();
-    }
-
-    public Optional<Member> findById(Long id){
-        Member member = em.find(Member.class, id);
-        return Optional.ofNullable(member);
-    }
-
-    public long count(){
-        return em.createQuery("select count(t) from Team t",long.class).getSingleResult();
-    }
+public interface TeamRepository extends JpaRepository<Team,Long> {
 }
