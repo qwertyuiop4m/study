a20cda3629b9253ec630e2a0cc941e90f89a434a
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-08-29 17:36:07 +0900

    spring data jpa paging

--- data-jpa/src/main/java/study/data_jpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -1,5 +1,8 @@
 package study.data_jpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -32,4 +35,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
      Member findMemberByUsername(String username);
 
      Optional<Member> findOptionalByUsername(String username);
+
+     Page<Member> findByAge(int age, Pageable pageable);
+
 }
