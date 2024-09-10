57babd2ea79f8267cf6e98b8fef0091b8fe7bf6b
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-09-10 16:05:00 +0900

    Web Extension-paging and sort

--- data-jpa/src/main/java/study/data_jpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/data_jpa/controller/MemberController.java
@@ -3,9 +3,12 @@ package study.data_jpa.controller;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
 import study.data_jpa.repository.MemberRepository;
 
@@ -26,8 +29,16 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(Pageable pageable){
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("userA"));
+        for (int i=0;i<100;i++){
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
