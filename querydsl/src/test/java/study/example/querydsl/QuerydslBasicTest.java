8cfbbe1797b0439cdb86f6fed3a24158e2693eda
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-09-25 19:00:10 +0900

    dynamicQuery-BooleanBuilder

--- querydsl/src/test/java/study/example/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/example/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.example.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -562,5 +563,29 @@ public class QuerydslBasicTest {
         }
     }
 
+    @Test
+    public void dynamicQurey_BooleanBuilder(){
+        String usernameParam="member1";
+        Integer ageParam=null;
 
+        List<Member> result=searchMember1(usernameParam,ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond!=null){
+            builder.and(member.username.eq(usernameCond));
+        }
+
+        if(ageCond!=null){
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
+    }
 }
