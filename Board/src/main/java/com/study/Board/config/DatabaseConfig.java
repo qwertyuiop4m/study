b60a5c5f870aabc91142c2f51055e7f1ccaceefb
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-11-20 21:26:49 +0900

    feat: XML Mapper 경로를 통해 찾아 읽어오는 코드

--- Board/src/main/java/com/study/Board/config/DatabaseConfig.java
+++ Board/src/main/java/com/study/Board/config/DatabaseConfig.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,5 +38,14 @@ public class DatabaseConfig {
     @Bean
     public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) {
         return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception{
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setMapperLocations(context.
+                getResources("classpath:/mappers/**/*Mapper.xml"));
+        return factoryBean.getObject();
     }
 }
