97290c3e49d9e43922edf559f3f604a004648f0b
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-11-21 00:07:12 +0900

    refactor: 오타 수정

--- Board/src/main/java/com/study/Board/config/DatabaseConfig.java
+++ Board/src/main/java/com/study/Board/config/DatabaseConfig.java
@@ -18,41 +18,43 @@ public class DatabaseConfig {
     @Autowired
     private ApplicationContext context;
 
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
     @Bean
     public DataSource dataSource() {
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/board?useSSL=false&characterEncoding=UTF-8&serverTimezone=UTC");
+        hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/board?serverTimezone=Asia/Seoul&useUnicode=true&characterEncoding=utf8&useSSL=false&allowPublicKeyRetrieval=true");
         hikariConfig.setUsername("root");
         hikariConfig.setPassword("password");
         HikariDataSource dataSource = new HikariDataSource(hikariConfig);
         return dataSource;
     }
 
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(@Autowired DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
-        sessionFactoryBean.setDataSource(dataSource);
-        return sessionFactoryBean.getObject();
-    }
-
-    @Bean
-    public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) {
-        return new SqlSessionTemplate(sqlSessionFactory);
-    }
 
     @Bean
     public SqlSessionFactory sqlSessionFactory() throws Exception{
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource());
-        factoryBean.setMapperLocations(context.
-                getResources("classpath:/mappers/**/*Mapper.xml"));
+        factoryBean.setMapperLocations(context.getResources("classpath:/mappers/**/*Mapper.xml"));
+        factoryBean.setConfiguration(mybatisConfig());
         return factoryBean.getObject();
+    }
+
+
+    @Bean
+    public SqlSessionTemplate sqlSession() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory());
     }
 
     @Bean
     @ConfigurationProperties(prefix = "mybatis.configuration")
-    public org.apache.ibatis.session.Configuration mybatisConfig(){
+    public org.apache.ibatis.session.Configuration mybatisConfig() {
         return new org.apache.ibatis.session.Configuration();
     }
 }
