8ce9fa5941413e903aa3366b4442002a0946966e
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-11-20 21:30:07 +0900

    feat: select 칼럼과 응답 클래스의 맴버 변수 매핑

--- Board/src/main/java/com/study/Board/config/DatabaseConfig.java
+++ Board/src/main/java/com/study/Board/config/DatabaseConfig.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -47,5 +48,11 @@ public class DatabaseConfig {
         factoryBean.setMapperLocations(context.
                 getResources("classpath:/mappers/**/*Mapper.xml"));
         return factoryBean.getObject();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "mybatis.configuration")
+    public org.apache.ibatis.session.Configuration mybatisConfig(){
+        return new org.apache.ibatis.session.Configuration();
     }
 }
