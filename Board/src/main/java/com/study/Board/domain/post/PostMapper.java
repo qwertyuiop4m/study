a0ebca6294a28c59675b99da475a8c3b38939991
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-11-25 22:43:21 +0900

    refactor: PostMapper 수정

--- Board/src/main/java/com/study/Board/domain/post/PostMapper.java
+++ Board/src/main/java/com/study/Board/domain/post/PostMapper.java
@@ -36,7 +36,7 @@ public interface PostMapper {
      * 게시글 리스트 조회
      * @return 게시글 리스트
      */
-    List<PostResponse> findAll();
+    List<PostResponse> findAll(SearchDto params);
 
     /**
      * 게시글 수 카운팅
