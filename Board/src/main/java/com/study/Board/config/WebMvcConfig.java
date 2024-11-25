34db81570971767000fc59261299c085f7056438
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-11-25 21:02:45 +0900

    feat: bean 등록

--- Board/src/main/java/com/study/Board/config/WebMvcConfig.java
+++ Board/src/main/java/com/study/Board/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.study.Board.config;
 
 import com.study.Board.interecptor.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
