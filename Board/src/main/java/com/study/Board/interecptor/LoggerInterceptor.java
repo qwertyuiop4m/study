b70b264cd749933b4412e1cf47cc930213aac2fa
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-11-25 20:54:59 +0900

    feat: LoggerInterceptor 추가 (HandlerInterceptor implements)

---
+++ Board/src/main/java/com/study/Board/interecptor/LoggerInterceptor.java
@@ -0,0 +1,27 @@
+package com.study.Board.interecptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerInterceptor implements HandlerInterceptor {
+    private static final Logger log=LoggerFactory.getLogger(LoggerInterceptor.class);
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("===============================================");
+        log.debug("==================== BEGIN ====================");
+        log.debug("Request URI ===> " + request.getRequestURI());
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.debug("==================== END ======================");
+        log.debug("===============================================");
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+}
