f3e91ca60f43b47bd3ecf20d1a39ebcd3ea5c3d7
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-11-25 20:57:37 +0900

    feat: WebMvcConfig 추가 (WebMvcConfigurer implements)

---
+++ Board/src/main/java/com/study/Board/config/WebMvcConfig.java
@@ -0,0 +1,13 @@
+package com.study.Board.config;
+
+import com.study.Board.interecptor.LoggerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor())
+                .excludePathPatterns("/css/**","/images/**","/js/**");
+    }
+}
