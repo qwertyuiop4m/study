d0393694f58aa0f5995a1e1060ef9c087fbf6b71
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-11-26 18:13:22 +0900

    refactor: PostService 수정

--- Board/src/main/java/com/study/Board/domain/post/PostService.java
+++ Board/src/main/java/com/study/Board/domain/post/PostService.java
@@ -15,43 +15,47 @@ public class PostService {
 
     /**
      * 게시글 저장
+     *
      * @param params - 게시글 정보
      * @return Generated PK
      */
     @Transactional
-    public Long savePost(final PostRequest params){
+    public Long savePost(final PostRequest params) {
         postMapper.save(params);
         return params.getId();
     }
 
     /**
      * 게시글 상세정보 조회
+     *
      * @param id - PK
      * @return 게시글 상세정보
      */
     @Transactional
-    public PostResponse findPostById(final Long id){
+    public PostResponse findPostById(final Long id) {
         return postMapper.findById(id);
     }
 
     /**
      * 게시글 수정
+     *
      * @param params - 게시글 정보
      * @return PK
      */
     @Transactional
-    public Long updatePost(final PostRequest params){
+    public Long updatePost(final PostRequest params) {
         postMapper.update(params);
         return params.getId();
     }
 
     /**
      * 게시글 삭제
+     *
      * @param id - PK
      * @return PK
      */
     @Transactional
-    public Long deletePost(final Long id){
+    public Long deletePost(final Long id) {
         postMapper.deleteById(id);
         return id;
     }
@@ -59,11 +63,12 @@ public class PostService {
 
     /**
      * 게시글 리스트 조회
+     *
      * @param params -search conditions
      * @return 게시글 리스트
      */
-    @Transactional
-    public List<PostResponse> findAllPost(final SearchDto params){
+    //@Transactional
+    public List<PostResponse> findAllPost(final SearchDto params) {
         return postMapper.findAll(params);
     }
 }
