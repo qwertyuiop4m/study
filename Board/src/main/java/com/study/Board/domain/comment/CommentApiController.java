fcde49dada226e4a4d7ef557f33fbf2ab2430095
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-11-27 22:59:15 +0900

    feat: comment 리스트 조회 추가

--- Board/src/main/java/com/study/Board/domain/comment/CommentApiController.java
+++ Board/src/main/java/com/study/Board/domain/comment/CommentApiController.java
@@ -3,6 +3,8 @@ package com.study.Board.domain.comment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class CommentApiController {
@@ -14,5 +16,11 @@ public class CommentApiController {
     public CommentResponse saveComment(@PathVariable final Long postId, @RequestBody final CommentRequest params){
         Long id= commentService.saveComment(params);
         return commentService.findCommentById(id);
+    }
+
+    //댓글 리스트 조회
+    @GetMapping("/posts/{postId}/comments")
+    public List<CommentResponse> findAllComment(@PathVariable final Long postId){
+        return commentService.findAllComment(postId);
     }
 }
