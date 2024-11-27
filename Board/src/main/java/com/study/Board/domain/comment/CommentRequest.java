b5734379a68d3513bf31dfea5679116e79f6e7c0
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2024-11-27 21:35:39 +0900

    feat: 댓글 request 클래스 생성

---
+++ Board/src/main/java/com/study/Board/domain/comment/CommentRequest.java
@@ -0,0 +1,14 @@
+package com.study.Board.domain.comment;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentRequest {
+    private Long id; //댓글 번호(Pk)
+    private Long postId; // 게시글 번호 (FK)
+    private String content; // 내용
+    private String writer; //작성자
+}
