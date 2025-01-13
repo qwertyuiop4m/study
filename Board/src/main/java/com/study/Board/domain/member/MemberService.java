74834bd8e959884de3902c4b2012a28ffcbc19d3
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2025-01-13 14:38:19 +0900

    feat: memberService

---
+++ Board/src/main/java/com/study/Board/domain/member/MemberService.java
@@ -0,0 +1,42 @@
+package com.study.Board.domain.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberMapper memberMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public Long saveMember(final MemberRequest params) {
+        params.encodingPassword(passwordEncoder);
+        memberMapper.save(params);
+        return params.getId();
+    }
+
+    public MemberResponse findMemberByLongId(final String loginId) {
+        return memberMapper.findByLoginId(loginId);
+    }
+
+    @Transactional
+    public Long updateMember(final MemberRequest params) {
+        params.encodingPassword(passwordEncoder);
+        memberMapper.update(params);
+        return params.getId();
+    }
+
+    @Transactional
+    public Long deleteMemberById(final Long id) {
+        memberMapper.deleteById(id);
+        return id;
+    }
+
+    public int countMemberByLoginId(final String loginId) {
+        return memberMapper.countByLoginId(loginId);
+    }
+}
