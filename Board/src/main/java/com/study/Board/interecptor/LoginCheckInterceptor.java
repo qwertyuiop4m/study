4b5d8669275e162027df79b5dcb197fb60166aa1
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2025-01-14 17:18:06 +0900

    feat: login session checking interceptor

---
+++ Board/src/main/java/com/study/Board/interecptor/LoginCheckInterceptor.java
@@ -0,0 +1,22 @@
+package com.study.Board.interecptor;
+
+import com.study.Board.domain.member.MemberResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        MemberResponse member=(MemberResponse)session.getAttribute("loginMember");
+
+        if (member == null|| member.getDeleteYn()) {
+            response.sendRedirect("/login.do");
+            return false;
+        }
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+}
