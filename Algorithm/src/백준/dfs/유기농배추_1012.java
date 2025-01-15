c2efa53308bed54d62b2054553dc2c1bb68fe75d
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2025-01-15 15:30:12 +0900

    코드 개선

--- Algorithm/src/백준/dfs/유기농배추_1012.java
+++ Algorithm/src/백준/dfs/유기농배추_1012.java
@@ -3,16 +3,13 @@ package 백준.dfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 
 public class 유기농배추_1012 {
     static boolean[][] visited;
     static int[][] map;
-    static List<Integer> list;
-    static int cnt=1;
+
     static int[] dx={1,-1,0,0};
     static int[] dy={0,0,1,-1};
     static int n,m;
@@ -21,10 +18,10 @@ public class 유기농배추_1012 {
         int T=Integer.parseInt(br.readLine());
         for (int t = 0; t < T; t++) {
             StringTokenizer st=new StringTokenizer(br.readLine());
+            int cnt=0;
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
-            list=new ArrayList<>();
             map = new int[m][n];
             visited = new boolean[m][n];
             for (int i=0;i<k;i++){
@@ -36,17 +33,15 @@ public class 유기농배추_1012 {
             for (int x = 0; x < m; x++) {
                 for (int y = 0; y < n; y++) {
                     if (map[x][y] == 1&&!visited[x][y]) {
-                        dfs(x,y);
-                        list.add(cnt);
-                        cnt=1;
+                        cnt+=dfs(x,y);
                     }
                 }
             }
-            System.out.println(list.size());
+            System.out.println(cnt);
         }
     }
 
-    private static void dfs(int x, int y) {
+    private static int dfs(int x, int y) {
         visited[x][y]=true;
         for (int i=0;i<4;i++) {
             int nx=x+dx[i];
@@ -56,8 +51,8 @@ public class 유기농배추_1012 {
             }
             if (map[nx][ny]==1&&!visited[nx][ny]) {
                 dfs(nx,ny);
-                cnt++;
             }
         }
+        return 1;
     }
 }
