b14e2a0eadc5ea89ce2c16a275681604fbdfbc22
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2025-01-22 15:50:14 +0900

    백준 11726

---
+++ Algorithm/src/백준/DP/타일링2xn_11726.java
@@ -0,0 +1,19 @@
+package 백준.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 타일링2xn_11726 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int[] dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=(dp[i-1]+dp[i-2])%1000000007;
+        }
+        System.out.println(dp[n]);
+    }
+}
