a7d11f3a602513da460493348d86a31758fce3af
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2025-01-22 16:03:40 +0900

    백준 11726

--- Algorithm/src/백준/DP/타일링2xn_11726.java
+++ Algorithm/src/백준/DP/타일링2xn_11726.java
@@ -5,15 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 타일링2xn_11726 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
-        int[] dp=new int[n+1];
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<=n;i++){
-            dp[i]=(dp[i-1]+dp[i-2])%1000000007;
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+            if (n == 1) {
+                System.out.println(1);
+                return;
+            }
+            int a = 1; // dp[1]
+            int b = 2; // dp[2]
+            for (int i = 3; i <= n; i++) {
+                int temp = (a + b) % 10007;
+                a = b;
+                b = temp;
+            }
+            System.out.println(b);
         }
-        System.out.println(dp[n]);
     }
 }
