4492ffea65c71e168593fc780c084a8294852584
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2025-03-27 18:52:00 +0900

    백준 2447

---
+++ Algorithm/src/백준/유형익히기/별찍기_2447.java
@@ -0,0 +1,31 @@
+package 백준.유형익히기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 별찍기_2447 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n=Integer.parseInt(br.readLine());
+    for(int i=0;i<n;i++){
+      for (int j=0;j<n;j++){
+        if(isBlank(i,j)){
+          System.out.print(" ");
+        }else{
+          System.out.print("*");
+        }
+      }
+      System.out.println();
+    }
+  }
+
+  private static boolean isBlank(int i, int j) {
+    while (i>0||j>0) {
+      if (i%3==1&&j%3==1)return true;
+      i/=3;
+      j/=3;
+    }
+    return false;
+  }
+}
